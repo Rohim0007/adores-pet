b5b7fbffe54c2ede0750d75c077bc58972f820ac
Rohim0007 <vdjrohim@gmail.com>
2026-08-21 20:06:18 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const SUPABASE_URL =
   "https://dghpfkcnyvddkmimfcmb.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  "তোমার sb_publishable_... key";
+  "sb_publishable_E_l44xOaicx1Ij02rturwQ_7I5Umq1Q";
